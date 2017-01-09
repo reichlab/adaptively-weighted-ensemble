83646333d31458d11371667399f5d12f767e2852
EvanLRay <Evan.L.Ray@gmail.com>
2017-01-09 12:42:53 -0500

    augment onset baselines provided by CDC with baseline values for seasons up through 2006-2007.  Not possible to compute baselines in earlier seasons due to missing data, so have just used region-specific means.

--- inst/data-processing/create-flu-baseline-file.R
+++ inst/data-processing/create-flu-baseline-file.R
@@ -13,4 +13,22 @@ dat <- as_data_frame(dat) %>%
               baseline=baseline)
 dat$season <- gsub("\\.", "/", dat$season) ## replaces . with / in season string
 
+## fill in baselines for seasons before 2007/2008 with means
+mean_region_baseline <- dat %>%
+  group_by(region) %>%
+  summarize(baseline = round(mean(baseline), 1))
+mean_region_baseline <- mean_region_baseline[c(1:2, 4:11, 3), ]
+
+regions <- c("National", paste0("Region", 1:10))
+for(first_season_year in 2006:1997) {
+  dat <- bind_rows(
+    data.frame(
+      region = regions,
+      season = paste0(first_season_year, "/", first_season_year + 1),
+      baseline = mean_region_baseline$baseline
+    ),
+    dat
+  )
+}
+
 write.csv(dat, "data-raw/cdc-baselines.csv", quote = FALSE, row.names = FALSE)
