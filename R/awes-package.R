03b16329c392a90f86786d23f2b4feb88a01e7d7
Nicholas Reich <nick@schoolph.umass.edu>
2017-01-11 11:43:14 -0500

    adding package documentation including useDynLib updates to namespace file.

---
+++ R/awes-package.R
@@ -0,0 +1,11 @@
+#'awes - Adaptively Weighted Ensembles via Stacking
+#'
+#'Tools for an implementation of adaptively weighted ensembles.
+#'
+#'@author Evan Ray <elray@umass.edu>
+#'@docType package
+#'@name awes
+#'@importFrom Rcpp evalCpp
+#'@useDynLib awes, .registration = TRUE
+#'@aliases awes awes-package
+NULL
