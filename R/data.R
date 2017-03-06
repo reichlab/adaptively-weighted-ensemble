c5cc80fa13ca791241bad153de72d5d9cfe7e871
Nicholas Reich <nick@schoolph.umass.edu>
2017-03-05 22:08:01 -0500

    adding data documentation (no perfect, still missing itemized list of columns). Still closes #18.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Influenza incidence in the US (1997 - 2016)
+#' 
+#' A dataset of public influenza data from the US CDC.
+#' 
+#' @format A data.frame with 11,055 observations on weighted influenza-like illness 
+#' measurements from all HHS regions, including the national level.
+#' @source The cdcfluview R package. 
+"flu_data"
+
+#' Flu season "onset thresholds" from the US CDC.
+#' 
+#' @format A data.frame with 220 observations of the seasonal baseline threshold used for 
+#' determining the "flu season onset" in each region of the US.
+#' @source \url{https://github.com/cdcepi/FluSight-forecasts/blob/master/wILI_Baseline.csv}
+"flu_onset_baselines"
