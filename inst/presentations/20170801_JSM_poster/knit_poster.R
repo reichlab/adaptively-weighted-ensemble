5b40f104b34c7dc410b1341ca111c8e19a6b97b1
elray1 <evan.l.ray@gmail.com>
2017-08-04 15:21:49 -0400

    add JSM poster files

---
+++ inst/presentations/20170801_JSM_poster/knit_poster.R
@@ -0,0 +1,4 @@
+setwd("~/Documents/research/epi/ensembles/adaptively-weighted-ensembles/inst/presentations/20170801_JSM_poster")
+library(knitr)
+knit2pdf("~/Documents/research/epi/ensembles/adaptively-weighted-ensembles/inst/presentations/20170801_JSM_poster/JSM2017_poster_ensembles.Rnw")
+
